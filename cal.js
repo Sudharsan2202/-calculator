27ea6a5cc966095a5a8b86f75cd36cd4b4b76606
Sudharsan2202 <iamsudhan.sudharsan2002@gmail.com>
2024-05-01 15:24:25 +0530

    cal

---
+++ cal.js
@@ -0,0 +1,20 @@
+let outputScreen =document.getElementById("output-screen");
+function display(num){
+   outputScreen.value += num;
+}
+
+function cal(){
+    try{
+       outputScreen.value =  eval(outputScreen.value);
+   }
+    catch(err)
+ {
+       alert("Invalid")
+    }
+}
+function cl(){
+   outputScreen.value = "";
+}
+function del(){
+    outputScreen.value = outputScreen.value.slice(0,-1);
+}
